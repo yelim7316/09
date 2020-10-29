542d7cd0086711b285e9c494c2683c12d5a69b01
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-10-29 15:44:50 +0900

    03

--- main.c
+++ main.c
@@ -4,25 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i, average;
-	int sum;
+	int i;
 	int grade[SIZE];
-	
-
-	printf("input %i scores\n", SIZE);
-	
-	sum = 0;
+	int score[SIZE];
 	
 	for(i=0;i<SIZE;i++){
-	
-		scanf("%d", &grade[i]);
-		sum += grade[i];
+	  grade[i]= rand()%100+1;
+				
 }
+
 	for(i=0;i<SIZE;i++)
-	    printf("grade[%d] =%d\n", i, grade[i]);
-	
-	average = sum/SIZE;
-	printf("average : %i\n", average);
+	   score[i] =grade[i];
+	   
+	for(i=0;i<SIZE;i++)  
+	    printf("score[%d] %i(%i)\n", i, score[i], grade[i]);
 	
 	return 0;
 }
